fcafda037d2840cece26704411637698b91f83df
Kunal Keshan <kunalkeshan12@gmail.com>
2025-04-10 03:34:57 +0530

    chore: update package.json and pnpm-lock.yaml, add maintenance mode schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -7,12 +7,13 @@ import testimonial from './schemas/testimonial';
 import gallery from './schemas/gallery';
 import team from './schemas/team';
 import labTests from './schemas/labTests';
+import partnerLab from './schemas/partnerLab';
+import maintenanceMode from './schemas/maintenanceMode';
 
 import blockContent from './schemas/blockContent';
 import { localeString } from './schemas/localeStringType';
 import { localeText } from './schemas/localeTextType';
 import { localeBlockContent } from './schemas/localeBlockContent';
-import partnerLab from './schemas/partnerLab';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
 	types: [
@@ -24,6 +25,7 @@ export const schema: { types: SchemaTypeDefinition[] } = {
 		team,
 		labTests,
 		partnerLab,
+		maintenanceMode,
 		blockContent,
 		localeString,
 		localeText,
---
+++ sanity/schemas/maintenanceMode.ts
@@ -0,0 +1,44 @@
+import { defineField, defineType } from "sanity";
+import { baseLanguage } from "../lib/locale";
+
+export default defineType({
+  name: "maintenance-mode",
+  title: "Maintenance Mode",
+  type: "document",
+  fields: [
+    defineField({
+      name: "isMaintenanceMode",
+      title: "Is Maintenance Mode",
+      type: "boolean",
+      description: "Whether the site is in maintenance mode.",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "maintenanceMessage",
+      title: "Maintenance Message",
+      type: "localeText",
+      description:
+        "The message to display to users when the site is in maintenance mode.",
+      validation: (Rule) => Rule.required(),
+    }),
+    defineField({
+      name: "disableMaintenanceModeBy",
+      title: "Disable Maintenance Mode By",
+      type: "datetime",
+      description: "The date and time the maintenance mode will be disabled.",
+      validation: (Rule) => Rule.required(),
+    }),
+  ],
+  preview: {
+    select: {
+      title: `isMaintenanceMode`,
+      subtitle: `maintenanceMessage.${baseLanguage?.id}`,
+    },
+    prepare({ title, subtitle }) {
+      return {
+        title,
+        subtitle,
+      };
+    },
+  },
+});
